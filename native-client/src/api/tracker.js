2d13d8c46e1f3a6eda8ace0c62f1a9b6d58b46af
adjohari <adjohari25@gmail.com>
2024-07-12 17:46:14 +0530

    add authorization header directly to the api

--- native-client/src/api/tracker.js
+++ native-client/src/api/tracker.js
@@ -1,5 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-export default axios.create({
+const trackApi = axios.create({
   baseURL: "https://d139-103-180-42-166.ngrok-free.app",
 });
+
+trackApi.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default trackApi;
