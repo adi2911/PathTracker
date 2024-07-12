f9082bbe0c89018dcf6b8e5f0d6ba0cdef6c00b9
adjohari <adjohari25@gmail.com>
2024-07-12 18:05:41 +0530

    TrackDetails Screen update

--- native-client/src/screens/TrackDetailScreen.js
+++ native-client/src/screens/TrackDetailScreen.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
+import { Context as TrackContext } from "../context/TrackContext";
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const _id = navigation.getParam("_id");
+
+  const { state } = useContext(TrackContext);
+
+  const track = state.find((tr) => tr._id === _id);
+  const initialCoords = track.locations[0].coords;
+
   return (
     <View>
-      <Text> TrackDetail Screen</Text>
+      <Text style={styles.trackName}> {track.name}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+      >
+        <Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+      </MapView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  trackName: { fontSize: 48 },
+  map: {
+    height: 300,
+  },
+});
 export default TrackDetailScreen;
